a35dc9f7c7107b201b7da72d03fe510ced9d9656
Cody Nicoll <cdnicoll@gmail.com>
2020-05-28 13:58:40 -0700

    save point

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = function (api) {
   api.cache(true)
 
-  const presets = ['@babel/preset-env', '@babel/preset-react']
-
+  const presets = ['@babel/env', '@babel/preset-react']
+  const plugins = ['@babel/plugin-external-helpers']
   return {
     presets,
+    plugins,
   }
 }
