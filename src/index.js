1b0b053ba794240b2b2f2cbceaed8b7b2220ef5b
Cody Nicoll <cdnicoll@gmail.com>
2020-06-09 14:21:13 -0700

    Added Sentry Transport

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import TextArea from './fields/TextArea'
 import SwitchInput from './fields/SwitchInput'
 import SelectInput from './fields/SelectInput'
 import CreateSelect from './fields/CreateSelect'
+// TODO: We're seeing an error with RichEditor around react-draft-wysiwyg
+// See here for more information: https://github.com/ocupop/4p-common/issues/14
 // import RichEditor from './fields/RichEditor'
 import NumberInput from './fields/NumberInput'
 import PhoneInput from './fields/PhoneInput'
