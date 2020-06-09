8e035089b08efd245b2519368ce899926eaf09fc
Cody Nicoll <cdnicoll@gmail.com>
2020-06-09 15:25:53 -0700

    Added UseLoader as a component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import TextArea from './fields/TextArea'
 import SwitchInput from './fields/SwitchInput'
 import SelectInput from './fields/SelectInput'
 import CreateSelect from './fields/CreateSelect'
-// TODO: We're seeing an error with RichEditor around react-draft-wysiwyg
-// See here for more information: https://github.com/ocupop/4p-common/issues/14
 import RichEditor from './fields/RichEditor'
 import NumberInput from './fields/NumberInput'
 import PhoneInput from './fields/PhoneInput'
 import EmailInput from './fields/EmailInput'
 import CurrencyInput from './fields/CurrencyInput'
 import SentryTransport from './common/logger/SentryTransport'
+import useLoading from './common/hooks/useLoading'
 
 export {
   TextInput,
@@ -24,4 +23,5 @@ export {
   EmailInput,
   CurrencyInput,
   SentryTransport,
+  useLoading,
 }
